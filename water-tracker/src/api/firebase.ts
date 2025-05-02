f28c66ce8dcba62c39e284d5ed98cf7f451853a2
EgorGer <QwertiQQQ@gmal.com>
2025-05-02 10:09:50 +0300

    feat: add fire base api

---
+++ water-tracker/src/api/firebase.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCtDbxHlTC-9UqaQWrS-HMETfV5pakVf3o",
+    authDomain: "water-tracker-6f8ca.firebaseapp.com",
+    projectId: "water-tracker-6f8ca",
+    storageBucket: "water-tracker-6f8ca.firebasestorage.app",
+    messagingSenderId: "504549086275",
+    appId: "1:504549086275:web:8e8de9603d52a80f9930f6",
+    measurementId: "G-EQSDZGL49G"
+  };
