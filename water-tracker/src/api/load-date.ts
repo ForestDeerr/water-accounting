4ab8f87f53f312a4e6d816adae6929d78c95ab69
EgorGer <QwertiQQQ@gmal.com>
2025-05-05 14:00:03 +0300

    feat: add load date to stor

--- water-tracker/src/api/load-date.ts
+++ water-tracker/src/api/load-date.ts
@@ -3,7 +3,7 @@ import { db } from './firebase';
 
 async function loadEmployees() {
   const snapshot = await get(child(ref(db), 'cashEntries'));
-  console.log(snapshot.val());
+  return snapshot.val();
 }
 
 export { loadEmployees };
