0a9d0dffe979328eaef6deded37773cf14150b75
EgorGer <QwertiQQQ@gmal.com>
2025-05-07 13:21:44 +0300

    feat: add edit page

---
+++ water-tracker/src/app/pages/edit-page/edit-page.ts
@@ -0,0 +1,13 @@
+import { Employees } from '../../../types/baseType';
+import { createHeader } from '../../header/header';
+
+function renderEditPage(employees: Employees) {
+  document.body.replaceChildren();
+
+  const container = document.createElement('div');
+  container.className = 'main-container';
+
+  container.append(createHeader());
+  return container;
+}
+export { renderEditPage };
