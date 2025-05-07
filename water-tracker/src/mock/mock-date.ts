5251a0a2e845055e0c9a9197360afdff1c0cc27c
EgorGer <QwertiQQQ@gmal.com>
2025-05-07 10:11:21 +0300

    feat: update mock date

--- water-tracker/src/mock/mock-date.ts
+++ water-tracker/src/mock/mock-date.ts
@@ -1,36 +1,37 @@
-import type { Employees } from "../types/baseType";
+import type { Employees } from '../types/baseType';
 
 export const mock: Employees = [
-    {
-      employeeId: '123',
-      employeeName: 'Иван Петров',
-      isActive: true,
-      isDelete: false,
-      transactions: [
-        { date: '2025-04-26', type: 'deposit', amount: 150 },
-        { date: '2025-04-27', type: 'expense', amount: 50 },
-      ],
-    },
-    {
-      employeeId: '124',
-      employeeName: 'Ирина Cидорова',
-      isActive: false,
-      isDelete: false,
-      transactions: [
-        { date: '2025-04-26', type: 'deposit', amount: 150 },
-        { date: '2025-04-27', type: 'expense', amount: 50 },
-        { date: '2025-04-28', type: 'expense', amount: 96 },
-      ],
-    },
-    {
-      employeeId: '125',
-      employeeName: 'Вася Иванов',
-      isActive: false,
-      isDelete: false,
-      transactions: [
-        { date: '2025-04-26', type: 'deposit', amount: 150 },
-        { date: '2025-04-27', type: 'expense', amount: 50 },
-        { date: '2025-04-28', type: 'expense', amount: 150 },
-      ],
-    },
-  ];
+  {
+    employeeId: '123',
+    employeeName: 'Иван Петров',
+    isActive: true,
+    isDelete: false,
+    transactions: [
+      { date: '2025-04-26', type: 'deposit', amount: 150 },
+      { date: '2025-04-27', type: 'expense', amount: 50 },
+    ],
+  },
+  {
+    employeeId: '124',
+    employeeName: 'Ирина Cидорова',
+    isActive: false,
+    isDelete: false,
+    transactions: [
+      { date: '2025-04-26', type: 'deposit', amount: 150 },
+      { date: '2025-04-27', type: 'expense', amount: 50 },
+      { date: '2025-04-28', type: 'expense', amount: 96 },
+    ],
+  },
+  {
+    employeeId: '125',
+    employeeName: 'Вася Иванов',
+    isActive: false,
+    isDelete: false,
+    transactions: [
+      { date: '2025-04-26', type: 'deposit', amount: 150 },
+      { date: '2025-04-27', type: 'expense', amount: 50 },
+      { date: '2025-04-28', type: 'expense', amount: 150 },
+      { date: '2025-04-28', type: 'deposit', amount: 20 },
+    ],
+  },
+];
