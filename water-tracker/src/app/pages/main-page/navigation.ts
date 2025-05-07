fde7a93e7a9f751b11ca8ed5223056fe8e6fc0e9
EgorGer <QwertiQQQ@gmal.com>
2025-05-07 11:04:00 +0300

    refactor: cheng navigation btn

--- water-tracker/src/app/pages/main-page/navigation.ts
+++ water-tracker/src/app/pages/main-page/navigation.ts
@@ -1,6 +1,28 @@
+import { createButton } from '../../utils/create-button';
+
 function navigationPanel(): HTMLElement {
   const container = document.createElement('div');
   container.className = 'navigation-panel';
+
+  const btnEdit = createButton({
+    type: 'button',
+    text: 'Редактировать',
+    className: 'btn-edit',
+    onClick: () => {
+      console.log('Редактировать');
+    },
+  });
+
+  const btnCalculate = createButton({
+    type: 'button',
+    text: 'Списать воду',
+    className: 'btn-edit',
+    onClick: () => {
+      console.log('Списать воду');
+    },
+  });
+
+  container.append(btnEdit, btnCalculate);
   return container;
 }
 
