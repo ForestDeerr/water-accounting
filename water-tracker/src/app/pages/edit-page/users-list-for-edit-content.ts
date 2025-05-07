b1cde4d66bf27edba29f2d639d74bc1b160ee83a
EgorGer <QwertiQQQ@gmal.com>
2025-05-07 14:49:11 +0300

    feat: add user list for edit page

---
+++ water-tracker/src/app/pages/edit-page/users-list-for-edit-content.ts
@@ -0,0 +1,12 @@
+import type { Employees } from '../../../types/baseType';
+
+function userListForContent(employees: Employees): HTMLElement {
+  const arr = Object.entries(employees).map(([id, obj]) => ({ id, ...obj }));
+
+  const container = document.createElement('div');
+  container.className = 'user-list-for-edit-page';
+
+
+  return container;
+}
+export { userListForContent };
