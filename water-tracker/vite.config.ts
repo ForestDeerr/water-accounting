78cc170829008c4653c3cbc3e9ae78cfccd1d189
EgorGer <QwertiQQQ@gmal.com>
2025-06-27 09:06:00 +0300

    feat: edit vite config

--- water-tracker/vite.config.ts
+++ water-tracker/vite.config.ts
@@ -1,5 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/water-accounting/',
-})
+  build: {
+    outDir: 'dist',
+  },
+  plugins: [
+    {
+      name: 'github-pages-spa-fallback',
+      closeBundle: async () => {
+        const fs = await import('fs/promises');
+        const path = await import('path');
+        const indexPath = path.resolve('dist/index.html');
+        const fallbackPath = path.resolve('dist/404.html');
+
+        try {
+          await fs.copyFile(indexPath, fallbackPath);
+        } catch (err) {
+          console.error('Failed to copy index.html to 404.html', err);
+        }
+      },
+    },
+  ],
+});
